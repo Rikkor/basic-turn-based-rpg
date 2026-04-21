7aa7e64ff99728125c533f222053732735522e52
Rikkor <psych4delic@gmail.com>
2026-04-21 02:26:12 -0700

    give the player sprite the ability to move via inputs

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+
+var speed = 250
+
+
+func _ready():
+	pass
+
+ # Called every frame
+func _process(delta):
+	PlayerMovement()
+
+# Handles moving the player
+func PlayerMovement():
+	velocity = Input.get_vector("left", "right", "up", "down")
+	
+	velocity = velocity.normalized() * speed
+	
+	move_and_slide()
