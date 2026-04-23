0a0c93d322b3cb50265c91cb278111c2cff01b46
Rikkor <psych4delic@gmail.com>
2026-04-23 03:08:49 -0700

    Adding in player direction

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+# Handles which direction the player moves/walks
+@onready var PlayerAnimator = get_node("AnimatedSprite2D")
 
 var speed = 250
-
+var direction = "face-right"
 
 func _ready():
 	pass
@@ -18,3 +20,16 @@ func PlayerMovement():
 	velocity = velocity.normalized() * speed
 	
 	move_and_slide()
+
+func PlayerAnimation():
+	if (velocity.x > 0):
+		direction = "face-right"
+	elif (velocity.x < 0):
+		direction = "face-left"
+	elif (velocity.y > 0):
+		direction = "face-down"
+	elif (velocity.y < 0):
+		direction = "face-up"
+	
+	if (velocity == Vector2.ZERO):
+		pass
