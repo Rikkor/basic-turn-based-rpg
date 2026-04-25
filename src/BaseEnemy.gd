14dceb23da6cf8551321e1661c64e8ba9a5618b5
Rikkor <psych4delic@gmail.com>
2026-04-25 01:01:36 -0700

    Setting up basic enemy

---
+++ src/BaseEnemy.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+@export var name : String = "Enemy"
+@export var texture : Texture = null
+@export var health : int = 30
+@export var damage : int = 5
+@export var minXP : int = 5
+@export var maxXP : int = 15
